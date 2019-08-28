8a7d7e9192993e50ee930b1b9695e2eff89c16e9
Varvara Babkova <varvara.munday@gmail.com>
2019-08-28 11:24:25 -0500

    Done.

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
@@ -40,9 +40,9 @@ export default class MasterHog extends Component {
         </div>
         
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          <BabyHog color = {this.state.eyeColor} data={offspring[0]}/>
+          <BabyHog color = {this.state.eyeColor} data={offspring[1]}/>
+          <BabyHog color = {this.state.eyeColor} data={offspring[2]}/>
         </ul>
 
       </div>
